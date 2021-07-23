048ca23f19c9edd8aa32abb2dfedbfff6c8fe3ef
Lucas <lucasho0629@gmail.com>
2021-07-23 12:06:19 +0800

    Adding Calculator

---
+++ calculator.js
@@ -0,0 +1,40 @@
+var calculateButton = document.querySelector(".calculateButton");
+
+calculateButton.addEventListener("click", handleClick);
+
+function handleClick() {
+	$(".spinner-grow").css("display", "inline-block");
+
+	var Lucas = 393.34;
+	var Bobo = 393.33;
+	var MY = 383.33;
+	var Derick = 483.33;
+	var Ricky = 746.67;
+	var Amanda = 0;
+
+	var EWBill = document.getElementById("Billing-01").value;
+	var SingtelBill = document.getElementById("Billing-02").value;
+	var BoboSingtel = document.getElementById("Billing-03").value;
+
+	var EWBill_each = EWBill / 5;
+	var SingtelBill_each = SingtelBill / 5;
+	var BoboSingtel_each = BoboSingtel / 1;
+
+	document.getElementById("Billing-01").value = EWBill_each;
+	document.getElementById("Billing-02").value = SingtelBill_each;
+	document.getElementById("Billing-03").value = BoboSingtel_each;
+
+	Lucas = (Lucas + EWBill_each + SingtelBill_each).toFixed(2);
+	Bobo = (Bobo + EWBill_each + SingtelBill_each + BoboSingtel_each).toFixed(2);
+	MY = (MY + EWBill_each + SingtelBill_each).toFixed(2);
+	Derick = (Derick + EWBill_each + SingtelBill_each).toFixed(2);
+	Ricky = (Ricky + EWBill_each + SingtelBill_each).toFixed(2);
+	Amanda = Amanda;
+
+	document.getElementById("Amount-01").innerHTML = Lucas;
+	document.getElementById("Amount-02").innerHTML = Bobo;
+	document.getElementById("Amount-03").innerHTML = MY;
+	document.getElementById("Amount-04").innerHTML = Derick;
+	document.getElementById("Amount-05").innerHTML = Ricky;
+	document.getElementById("Amount-06").innerHTML = Amanda;
+}
--- index.js
+++ index.js
@@ -3,73 +3,71 @@ var randomButton = document.querySelector(".randomButton");
 randomButton.addEventListener("click", handleClick);
 
 function handleClick() {
-  $(".spinner-grow").css("display", "inline-block");
+	$(".spinner-grow").css("display", "inline-block");
 
-  localStorage.setItem("Lucas", $('#Lucas').is(":checked"));
-  localStorage.setItem("Ricky", $('#Ricky').is(":checked"));
-  localStorage.setItem("BigYvonne", $('#BigYvonne').is(":checked"));
-  localStorage.setItem("Victor", $('#Victor').is(":checked"));
-  localStorage.setItem("Eason", $('#Eason').is(":checked"));
-  localStorage.setItem("SmallYvonne", $('#SmallYvonne').is(":checked"));
-  localStorage.setItem("KaiXin", $('#KaiXin').is(":checked"));
-  localStorage.setItem("Roy", $('#Roy').is(":checked"));
+	localStorage.setItem("Lucas", $("#Lucas").is(":checked"));
+	localStorage.setItem("Ricky", $("#Ricky").is(":checked"));
+	localStorage.setItem("BigYvonne", $("#BigYvonne").is(":checked"));
+	localStorage.setItem("Victor", $("#Victor").is(":checked"));
+	localStorage.setItem("Eason", $("#Eason").is(":checked"));
+	localStorage.setItem("SmallYvonne", $("#SmallYvonne").is(":checked"));
+	localStorage.setItem("KaiXin", $("#KaiXin").is(":checked"));
+	localStorage.setItem("Roy", $("#Roy").is(":checked"));
 
-  // window.location.href = "result.html";
+	// window.location.href = "result.html";
 
-  var whoisplaying = ["Lucas", "Ricky", "Eason", "Victor"];
-  var whoisplaying2 = ["Yvonne Lee", "Roy", "Yvonne Lim", "KaiXin"];
+	var whoisplaying = ["Lucas", "Ricky", "Eason", "Victor"];
+	var whoisplaying2 = ["Yvonne Lee", "Roy", "Yvonne Lim", "KaiXin"];
 
-  if ($('#Lucas').is(":checked") == false) {
-    whoisplaying[0] = "Empty";
-  }
-  if ($('#Ricky').is(":checked") == false) {
-    whoisplaying[1] = "Empty";
-  }
-  if ($('#Eason').is(":checked") == false) {
-    whoisplaying[2] = "Empty";
-  }
-  if ($('#Victor').is(":checked") == false) {
-    whoisplaying[3] = "Empty";
-  }
+	if ($("#Lucas").is(":checked") == false) {
+		whoisplaying[0] = "Empty";
+	}
+	if ($("#Ricky").is(":checked") == false) {
+		whoisplaying[1] = "Empty";
+	}
+	if ($("#Eason").is(":checked") == false) {
+		whoisplaying[2] = "Empty";
+	}
+	if ($("#Victor").is(":checked") == false) {
+		whoisplaying[3] = "Empty";
+	}
 
-  if ($('#BigYvonne').is(":checked") == false) {
-    whoisplaying2[0] = "Empty";
-  }
-  if ($('#Roy').is(":checked") == false) {
-    whoisplaying2[1] = "Empty";
-  }
-  if ($('#SmallYvonne').is(":checked") == false) {
-    whoisplaying2[2] = "Empty";
-  }
-  if ($('#KaiXin').is(":checked") == false) {
-    whoisplaying2[3] = "Empty";
-  }
+	if ($("#BigYvonne").is(":checked") == false) {
+		whoisplaying2[0] = "Empty";
+	}
+	if ($("#Roy").is(":checked") == false) {
+		whoisplaying2[1] = "Empty";
+	}
+	if ($("#SmallYvonne").is(":checked") == false) {
+		whoisplaying2[2] = "Empty";
+	}
+	if ($("#KaiXin").is(":checked") == false) {
+		whoisplaying2[3] = "Empty";
+	}
 
-  setTimeout(function() {
-    $(".spinner-grow").css("display", "none");
+	setTimeout(function () {
+		$(".spinner-grow").css("display", "none");
 
-    for (var a = whoisplaying, i = a.length; i--;) {
-      var counting = 0;
-      var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
-      var randomPointer = a.length + 1;
+		for (var a = whoisplaying, i = a.length; i--; ) {
+			var counting = 0;
+			var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+			var randomPointer = a.length + 1;
 
-      if (randomPointer % 2 == 0) {
-        $('#A-0' + randomPointer).text(random);
-      } else {
-        $('#A-0' + randomPointer).text(random);
-      }
-    }
-    for (var a = whoisplaying2, i = a.length; i--;) {
-      var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
-      var randomPointer = a.length + 1;
+			if (randomPointer % 2 == 0) {
+				$("#A-0" + randomPointer).text(random);
+			} else {
+				$("#A-0" + randomPointer).text(random);
+			}
+		}
+		for (var a = whoisplaying2, i = a.length; i--; ) {
+			var random = a.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+			var randomPointer = a.length + 1;
 
-      if (randomPointer % 2 == 0) {
-        $('#B-0' + randomPointer).text(random);
-      } else {
-        $('#B-0' + randomPointer).text(random);
-      }
-    }
-
-  }, 3000);
-
+			if (randomPointer % 2 == 0) {
+				$("#B-0" + randomPointer).text(random);
+			} else {
+				$("#B-0" + randomPointer).text(random);
+			}
+		}
+	}, 3000);
 }
