7693699ba799960efd5bcdc81d4a89c864de2c07
Lucas Ho <lucasho0629@gmail.com>
2021-10-25 10:14:45 +0800

    Amanda in

--- calculator.js
+++ calculator.js
@@ -9,15 +9,15 @@ function handleClick() {
 	var Bobo = 393.33;
 	var MY = 383.33;
 	var Derick = 483.33;
-	var Ricky = 746.67;
-	var Amanda = 0;
+	var Ricky = 373.34;
+	var Amanda = 373.33;
 
 	var EWBill = document.getElementById("Billing-01").value;
 	var SingtelBill = document.getElementById("Billing-02").value;
 	var BoboSingtel = document.getElementById("Billing-03").value;
 
 	var EWBill_each = EWBill / 5;
-	var SingtelBill_each = SingtelBill / 5;
+	var SingtelBill_each = SingtelBill / 6;
 	var BoboSingtel_each = BoboSingtel / 1;
 
 	document.getElementById("Billing-01").value = EWBill_each;
@@ -29,7 +29,9 @@ function handleClick() {
 	MY = (MY + EWBill_each + SingtelBill_each).toFixed(2);
 	Derick = (Derick + EWBill_each + SingtelBill_each).toFixed(2);
 	Ricky = (Ricky + EWBill_each + SingtelBill_each - 69.9).toFixed(2);
-	Amanda = Amanda;
+	Amanda = (Amanda + SingtelBill_each).toFixed(2);
+	Ricky = Ricky / 1 + Amanda / 1;
+	Amanda = 0;
 
 	document.getElementById("Amount-01").innerHTML = Lucas;
 	document.getElementById("Amount-02").innerHTML = Bobo;
